ddc2245e6feae99909d8238e8837d9491ad9e3c0
jcasas5253 <skedabble@gmail.com>
2018-11-08 15:42:32 -0800

    set all to 0 and added computerChoices array for the computer to choice from

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,8 @@
-
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessesSoFar = [];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 
