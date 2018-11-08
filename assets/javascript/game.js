12a7a31833b967a41e5ec8bce40125d8632f2a43
jcasas5253 <skedabble@gmail.com>
2018-11-08 13:55:58 -0800

    added variable for the computers choises to pick from

---
+++ assets/javascript/game.js
@@ -0,0 +1,2 @@
+//Choises for the computer to make
+var robotChoises = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
