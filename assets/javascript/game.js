c0cc5bbafa630671cb0ca772f86ed572ce7a11b8
jcasas5253 <skedabble@gmail.com>
2018-11-08 16:06:08 -0800

    added onkeyup function, added computer random choice and added case sensitivity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,22 @@
+//set all to 0
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var guessesSoFar = [];
+
+//Choices for the computer to choice from
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+document.onkeyup = function(event) {
+//computer randomly picks letter
+var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+//case sensetive
+var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+}
+
+
+
+
 
 
 
