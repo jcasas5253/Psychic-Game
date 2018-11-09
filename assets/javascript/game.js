d6721187d6dab01873caddb460a61d82ae098ef2
jcasas5253 <skedabble@gmail.com>
2018-11-08 16:38:53 -0800

    added Sound effects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,10 @@ var losses = 0;
 var guessesLeft = 10;
 var guessesSoFar = [];
 
+//audio
+var winner = new Audio("./assets/sounds/Ta Da-SoundBible.com-1884170640.mp3");
+var looser = new Audio("./assets/sounds/Sad_Trombone-Joe_Lamb-665429450.mp3");
+
 //Choices for the computer to choice from
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -14,19 +18,21 @@ var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.len
 var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 //pushing user guesses to the users guesses thus far
 guessesSoFar.push(userGuess);
-
+//if the user wins game resets and they get alert
 if (userGuess == computerGuess) {
     wins++;
     alert('Good Job Detective! You Win!')
     guessesLeft = 10;
     guessesSoFar.length = 0;
 }
+//if user looses game resets and they get alert
 else if (guessesLeft == 0) {
     losses++;
     alert('Oh No! Looks like you need more training.')
     guessesLeft = 10;
     guessesSoFar.length = 0;
 }
+//subtract users guesses
 else if (userGuess !== computerGuess){
     guessesLeft--;
 }
